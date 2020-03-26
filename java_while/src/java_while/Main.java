2fea7dd6be7d08c5c1f447dd43a3cdad6d9f26e3
Stephanie Castro <stephanie.castro@inmetrics.com.br>
2020-03-25 21:43:15 -0300

    projeto25-03

--- java_while/src/java_while/Main.java
+++ java_while/src/java_while/Main.java
@@ -1,9 +1,27 @@
 package java_while;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			
+			while (x != y) {
+				if (x <= y) {
+					System.out.println("CRESCENTE");
+				}
+			else {
+				System.out.println("DECRESCENTE");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();	
+		}
+		
+		sc.close();
 		
 	}
 }
